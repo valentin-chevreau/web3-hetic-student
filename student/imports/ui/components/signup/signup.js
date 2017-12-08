6faf521200778b4580b76f9ceb5e6333dd1f2b1c
SATIE\Constantin <constantin.burger@sooyoos.com>
2017-12-08 10:51:30 +0100

    Composant Signup
    Ajout du formulaire 'signup', pas de lien à la BDD

---
+++ student/imports/ui/components/signup/signup.js
@@ -0,0 +1,32 @@
+// import { Links } from '/imports/api/links/links.js';
+import { Meteor } from 'meteor/meteor';
+import './signup.html';
+
+// Template.info.onCreated(function () {
+//   Meteor.subscribe('links.all');
+// });
+//
+// Template.info.helpers({
+//   links() {
+//     return Links.find({});
+//   },
+// });
+//
+// Template.signup.events({
+//   'submit .add-student'(event) {
+//     event.preventDefault();
+//
+//     const target = event.target;
+//     const firstname = target.firetname;
+//     const lastname = target.lastname;
+//
+//     Meteor.call('links.insert', firstname.value, lastname.value, (error) => {
+//       if (error) {
+//         alert(error.error);
+//       } else {
+//         firstname.value = '';
+//         lastname.value = '';
+//       }
+//     });
+//   },
+// });
--- student/imports/ui/pages/home/home.js
+++ student/imports/ui/pages/home/home.js
@@ -1,4 +1,5 @@
 import './home.html';
 
 import '../../components/hello/hello.js';
-import '../../components/info/info.js';
+// import '../../components/info/info.js';
+import '../../components/signup/signup.js';
